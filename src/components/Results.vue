e683bf735adcb8c0c918ca843d237c0f3be456e8
surya-bug <77871514+surya-bug@users.noreply.github.com>
2021-01-24 16:06:13 +0530

    Added the project files

---
+++ src/components/Results.vue
@@ -0,0 +1,52 @@
+<template>
+  
+  <p> ReactionTime : {{score}} ms </p>
+
+  <p class="rank"> {{ rank }} </p>
+</template>
+
+<script>
+export default {
+props: ['score'],
+
+data(){
+
+    return {
+
+        rank:null,
+
+
+    }
+},
+
+mounted(){
+
+    if(this.score  < 250){
+
+        this.rank = "Ninja speeed "
+
+    }else if(this.score < 400 ){
+
+        this.rank = "You are a quickie!"
+    }else{
+        this.rank = "As slow as a snail !"
+    }
+
+
+    }
+}
+
+
+
+</script>
+
+<style>
+
+.rank {
+
+    font-size: 1.4em;
+    color: crimson;
+    font-weight: bold;
+}
+
+</style>
